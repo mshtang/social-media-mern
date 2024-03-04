f2f09298df6c79676851608d43c11f216204e67b
mshtang <32868278+mshtang@users.noreply.github.com>
2024-03-04 17:51:39 +0100

    add login controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,9 +1,9 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import User from "../models/User";
+import User from "../models/User.js";
 
 // register user
-export default register = async (req, res) => {
+export const register = async (req, res) => {
   try {
     const {
       firstName,
@@ -35,6 +35,24 @@ export default register = async (req, res) => {
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
   } catch (err) {
-    res.status(500).json({error: err.message});
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// logging in
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "User does not exist" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 };
