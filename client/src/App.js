e5736bf1eaa66e4b453cf859f9c8fd42150415dd
mshtang <32868278+mshtang@users.noreply.github.com>
2024-03-25 15:04:32 +0100

    add color tokens

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,28 @@ import "./App.css";
 import LoginPage from "scenes/loginPage";
 import HomePage from "scenes/homePage";
 import ProfilePage from "scenes/profilePage";
+import { useSelector } from "react-redux";
+import { useMemo } from "react";
+import { createTheme } from "@mui/system";
+import { themeSettings } from "theme";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
